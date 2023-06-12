162f45944fe7537841c4d55b60a24eb56786b33c
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-12 09:21:04 -0300

    feat: Adicionar decorador 'ParamId' para obter o parâmetro de ID

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,13 +3,12 @@ import {
 	Controller,
 	Delete,
 	Get,
-	Param,
-	ParseIntPipe,
 	Patch,
 	Post,
 	Put,
-	UseInterceptors,
+	UseInterceptors
 } from '@nestjs/common';
+import { ParamId } from '../decorators/param-id.decorator';
 import { LogInterceptor } from '../interceptors/log-interceptor';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UpdatePatchUserDto } from './dto/update-patch-user.dto';
@@ -29,20 +28,21 @@ export class UserController {
     return this.userService.list();
   }
   @Get(':id')
-  async show(@Param('id', ParseIntPipe) id: number) {
+  async show(@ParamId() id: number) {
+    console.log({ id });
     return this.userService.show(id);
   }
   @Put(':id')
   async update(
     @Body() { name, password, email, birthAt }: UpdatePutUserDto,
-    @Param('id', ParseIntPipe) id: number,
+    @ParamId() id: number,
   ) {
     return this.userService.update(id, { name, password, email, birthAt });
   }
   @Patch(':id')
   async updatePartial(
     @Body() { name, password, email, birthAt }: UpdatePatchUserDto,
-    @Param('id', ParseIntPipe) id: number,
+    @ParamId() id: number,
   ) {
     return this.userService.updatePartial(id, {
       name,
@@ -52,7 +52,7 @@ export class UserController {
     });
   }
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number) {
+  async delete(@ParamId() id: number) {
     return this.userService.delete(id);
   }
 }
